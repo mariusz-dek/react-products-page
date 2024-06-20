0b288286cde8abf4be55a17577a2b732e5e5d7be
Mariusz Dekarski <mariusz.dekarski@gmail.com>
2024-06-20 10:28:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { format, parseISO } from 'date-fns';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    fetch('products.json', {
+      crossDomain: true,
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: 'Basic ZXNfZGV2X29mZXJ0aWE6VVpXbW1IMkhSV3g1',
+        'Access-Control-Allow-Headers':
+          'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson.offer);
+        setData(myJson.offer);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='header'> Cashback Products</div>
+      <div className='grid auto-fill'>
+        {data &&
+          data.length > 0 &&
+          data.map((item) => (
+            <div key={item.displayName}>
+              <div>
+                <b>
+                  {item.brand.name} -{' '}
+                  {item.offerProductCategories.offerProductCategory[0].name}
+                </b>
+              </div>
+              <div>{item.displayName}</div>
+
+              <div>
+                {format(parseISO(item.startOn), 'dd/MM/yyyy')} -{' '}
+                {format(parseISO(item.endOn), 'dd/MM/yyyy')}
+              </div>
+              <img src={item.imageUrl} alt={item.displayName} />
+              <div className='price'>{item.displayValue}</div>
+              <div>{item.limit.individual.quantity} ud/usuario</div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
 
